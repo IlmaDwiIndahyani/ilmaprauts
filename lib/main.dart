b28815c727542fc572e4bfeb2a450bf75c5dbe2c
FlutLab.io <>
2022-05-25 04:25:54 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
+  runApp(MyApp())
+  
+};
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ListViewHome(),
+      home:MyHomePage(),
+      // about:About(),
+      // profile:Profile(),
     );
   }
 }
-
-class ListViewHome extends StatelessWidget {
-  var date = DateTime.now();
-  Widget build(BuildContext context) {
+class MyHomePage extends StatelessWidget {
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Toko Online'),
+        title: Text ('Belajar Mobile Programming'),
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(30, (index) {
-          return Container(
-            child: Card(
-              color: Colors.deepPurpleAccent,
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage('https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/12/05/838258459.jpeg'),
-                    fit: BoxFit.fitHeight,
-                  ),
-                ),
-                child: ListTile(
-                  title: Text(date.day.toString() + '/' + date.month.toString() + '/' + date.year.toString(), style: TextStyle(color: Colors.white, fontSize: 24.0)),
-                ),
-              ),
-            ),
-          );
-        }),
-      ),
-    );
+      body: Center(
+        child: Text('Selamat Datang Di Pembelajaran Mobile Programming'
+        style: TextStyle(
+          fontSize: 40,
+          fontFamily: 'DancingScript'
+        ))
+      )
+    )
   }
+
 }
