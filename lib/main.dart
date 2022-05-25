12c6ea85a93c5a286c5dbcab65a71a811360e99a
FlutLab.io <>
2022-05-25 05:26:10 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mobile Programming'),
+        title: Text('Toko Online'),
       ),
 
       // body: Center(
@@ -34,7 +34,7 @@ class MyHomePage extends StatelessWidget {
             decoration: BoxDecoration(
               color: const Color(0xff7c94b6),
               image: const DecorationImage(
-                image: NetworkImage('https://i.postimg.cc/Y9DWs4vB/IMG20210919095853.jpg'),
+                image: NetworkImage('https://cf.shopee.co.id/file/52d6a737fede49a1282b6c2371c7cab1'),
                 fit: BoxFit.cover,
               ),
               border: Border.all(
@@ -50,7 +50,7 @@ class MyHomePage extends StatelessWidget {
             decoration: BoxDecoration(
               color: const Color(0xff7c94b6),
               image: const DecorationImage(
-                image: NetworkImage('https://i.postimg.cc/WbbyZxvD/IMG20210919095817.jpg'),
+                image: NetworkImage('https://images.tokopedia.net/img/cache/700/product-1/2019/6/18/6189071/6189071_ab043b81-9a41-4282-9dae-17879866bd15_800_800.jpg'),
                 fit: BoxFit.cover,
               ),
               border: Border.all(
@@ -84,7 +84,7 @@ class MyHomePage extends StatelessWidget {
             decoration: BoxDecoration(
               color: const Color(0xff7c94b6),
               image: const DecorationImage(
-                image: NetworkImage('https://i.postimg.cc/WbbyZxvD/IMG20210919095817.jpg'),
+                image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnf9eg7ituaI3lq2LMu6PCDF6sCBMZqTA-kw&usqp=CAU'),
                 fit: BoxFit.cover,
               ),
               border: Border.all(
@@ -118,7 +118,7 @@ class MyHomePage extends StatelessWidget {
             decoration: BoxDecoration(
               color: const Color(0xff7c94b6),
               image: const DecorationImage(
-                image: NetworkImage('https://i.postimg.cc/WbbyZxvD/IMG20210919095817.jpg'),
+                image: NetworkImage('https://foto.kontan.co.id/fZsIkDH9S6hQxziElfC_1x_5otE=/smart/2020/07/03/334318002p.jpg'),
                 fit: BoxFit.cover,
               ),
               border: Border.all(
