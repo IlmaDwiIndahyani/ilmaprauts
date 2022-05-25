2b927169549c7107f3d930ad759c743bbec55ab8
FlutLab.io <>
2022-05-25 04:50:06 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,137 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: BelajarStackWidget(),
-  ));
+  runApp(MyApp());
 }
 
-class BelajarStackWidget extends StatefulWidget {
-  @override
-  _BelajarStackWidgetState createState() => _BelajarStackWidgetState();
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      // about:About(),
+      // profile:Profile(),
+    );
+  }
 }
 
-class _BelajarStackWidgetState extends State<BelajarStackWidget> {
-  var date = DateTime.now();
-
-  @override
+class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
+      appBar: AppBar(
+        title: Text('Mobile Programming'),
+      ),
+
+      // body: Center(
+      //   child: Text('Selamat Datang Di Pembelajaran Mobile Programming'
+      //   style: TextStyle(
+      //     fontSize: 40,
+      //     fontFamily: 'DancingScript'
+      //   ))
+      // )
+      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
           Container(
             decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage('https://i.postimg.cc/pLQMc0sg/bikjmol.jpg'),
-                fit: BoxFit.fitHeight,
+              color: const Color(0xff7c94b6),
+              image: const DecorationImage(
+                image: NetworkImage('https://i.postimg.cc/Y9DWs4vB/IMG20210919095853.jpg'),
+                fit: BoxFit.cover,
               ),
-            ),
-          ),
-          Positioned(
-            right: 200.0,
-            top: 100.0,
-            child: Text(date.hour.toString() + ':' + date.minute.toString(), style: TextStyle(color: Colors.white, fontSize: 60.0)),
-          ),
-          Positioned(
-            right: 200.0,
-            top: 170.0,
-            child: Text("Senin,23 Mei 2022", style: TextStyle(color: Colors.white, fontSize: 16.0)),
-          ),
-          Positioned(
-            bottom: 48.0,
-            left: 10.0,
-            right: 10.0,
-            child: Card(
-              elevation: 8.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
               ),
+              borderRadius: BorderRadius.circular(12),
             ),
+            height: 150,
+            width: 150,
           ),
-        ],
-      ),
+          Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
+              image: const DecorationImage(
+                image: NetworkImage('https://i.postimg.cc/WbbyZxvD/IMG20210919095817.jpg'),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            height: 150,
+            width: 150,
+          )
+        ]),
+        new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
+              image: const DecorationImage(
+                image: NetworkImage('https://i.postimg.cc/Y9DWs4vB/IMG20210919095853.jpg'),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            height: 150,
+            width: 150,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
+              image: const DecorationImage(
+                image: NetworkImage('https://i.postimg.cc/WbbyZxvD/IMG20210919095817.jpg'),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            height: 150,
+            width: 150,
+          )
+        ]),
+        new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
+              image: const DecorationImage(
+                image: NetworkImage('https://i.postimg.cc/Y9DWs4vB/IMG20210919095853.jpg'),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            height: 150,
+            width: 150,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
+              image: const DecorationImage(
+                image: NetworkImage('https://i.postimg.cc/WbbyZxvD/IMG20210919095817.jpg'),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            height: 150,
+            width: 150,
+          )
+        ])
+      ]),
     );
   }
 }
