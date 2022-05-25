62c2b90f33afcd19b013b32c1a54e72e1b8c8451
FlutLab.io <>
2022-05-25 04:30:35 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: BelajarStackWidget(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class BelajarStackWidget extends StatefulWidget {
+  @override
+  _BelajarStackWidgetState createState() => _BelajarStackWidgetState();
+}
+
+class _BelajarStackWidgetState extends State<BelajarStackWidget> {
+  var date = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ListViewHome(),
-    );
-  }
-}
-
-class ListViewHome extends StatelessWidget {
-  var date = DateTime.now();
-  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Toko Online'),
-      ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(30, (index) {
-          return Container(
-            child: Card(
-              color: Colors.deepPurpleAccent,
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage('https://cf.shopee.co.id/file/52d6a737fede49a1282b6c2371c7cab1'),
-                    fit: BoxFit.fitHeight,
-                  ),
-                ),
-                child: ListTile(
-                  title: Text(date.day.toString() + '/' + date.month.toString() + '/' + date.year.toString(), style: TextStyle(color: Colors.white, fontSize: 24.0)),
-                ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage('https://i.postimg.cc/pLQMc0sg/bikjmol.jpg'),
+                fit: BoxFit.fitHeight,
               ),
             ),
-          );
-        }),
+          ),
+          Positioned(
+            right: 200.0,
+            top: 100.0,
+            child: Text(date.hour.toString() + ':' + date.minute.toString(), style: TextStyle(color: Colors.white, fontSize: 60.0)),
+          ),
+          Positioned(
+            right: 200.0,
+            top: 170.0,
+            child: Text("Senin,23 Mei 2022", style: TextStyle(color: Colors.white, fontSize: 16.0)),
+          ),
+          Positioned(
+            bottom: 48.0,
+            left: 10.0,
+            right: 10.0,
+            child: Card(
+              elevation: 8.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
