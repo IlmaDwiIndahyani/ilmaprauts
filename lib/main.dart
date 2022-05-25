cc73c0e5c0ffa8bcc8661240eca7ffe8ece303c9
FlutLab.io <>
2022-05-25 07:50:27 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,133 +5,57 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Toko Online ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: MyHomePage(),
-      // about:About(),
-      // profile:Profile(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final List galery = [
+    "assets/images/img1.jpg",
+    "assets/images/img2.jpg",
+    "assets/images/img3.jpg",
+    "assets/images/img4.jpg",
+    "assets/images/img5.jpg",
+    "assets/images/img6.jpg",
+    "assets/images/img7.jpg",
+    "assets/images/img8.jpg",
+    "assets/images/img9.jpg",
+    "assets/images/img10.jpg",
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Toko Online'),
+        title: Text("Toko Online", style: TextStyle(fontWeight: FontWeight.bold)),
       ),
-
-      // body: Center(
-      //   child: Text('Toko Online'
-      //   style: TextStyle(
-      //     fontSize: 40,
-      //     fontFamily: 'DancingScript'
-      //   ))
-      // )
-      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: NetworkImage('https://cf.shopee.co.id/file/52d6a737fede49a1282b6c2371c7cab1'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(
-                color: Colors.grey,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            height: 130,
-            width: 130,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: NetworkImage('https://images.tokopedia.net/img/cache/700/product-1/2019/6/18/6189071/6189071_ab043b81-9a41-4282-9dae-17879866bd15_800_800.jpg'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(
-                color: Colors.grey,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            height: 130,
-            width: 130,
-          )
-        ]),
-        new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: NetworkImage('https://img.beritasatu.com/cache/investor/798x449-2/1623225210.jpg'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(
-                color: Colors.grey,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            height: 130,
-            width: 130,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnf9eg7ituaI3lq2LMu6PCDF6sCBMZqTA-kw&usqp=CAU'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(
-                color: Colors.grey,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            height: 130,
-            width: 130,
-          )
-        ]),
-        new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: NetworkImage('https://i.pinimg.com/550x/25/c9/a1/25c9a155dc6c2f71912548bbf2101ffb.jpg'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(
-                color: Colors.grey,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            height: 130,
-            width: 130,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: NetworkImage('https://foto.kontan.co.id/fZsIkDH9S6hQxziElfC_1x_5otE=/smart/2020/07/03/334318002p.jpg'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(
-                color: Colors.grey,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            height: 130,
-            width: 130,
-          )
-        ])
-      ]),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          if (index > 2) {
+            return Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Image.asset(galery[index % 15]),
+            );
+          } else {
+            return Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Image.asset(galery[index]),
+            );
+          }
+        },
+      ),
     );
   }
 }
